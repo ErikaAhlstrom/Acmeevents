c5972ceb52f2331864494a8252edf65228d132cd
GustafJ <gustaf89@hotmail.se>
2020-10-19 16:17:01 +0200

    worked with the classes

---
+++ code/event.js
@@ -0,0 +1,23 @@
+class Event {
+  constructor(
+    name = "unknown",
+    start = "unknown",
+    end = "unknown",
+    time = "unknown",
+    category = "unknown",
+    location = "unknown",
+    eventOwner = "unknown",
+    admin = "unknown",
+    participants = "unknown"
+  ) {
+    this.name = name;
+    this.start = start;
+    this.end = end;
+    this.time = time;
+    this.category = category;
+    this.location = location;
+    this.eventOwner = eventOwner;
+    this.admin = admin;
+    this.participants = participants;
+  }
+}
