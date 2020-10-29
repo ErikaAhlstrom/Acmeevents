4516b3de89a6f6a73e8d95784c8d7d5aeab7bcf3
“erikaahlstrom” <info@erikasoldh.se>
2020-10-29 18:21:49 +0100

    jobbat med öppna kort på main page

--- code/EventList/scripts/eventList.js
+++ code/EventList/scripts/eventList.js
@@ -13,8 +13,136 @@ class itemLister{
         this.filterStartDate.value = ""
         this.filterEndDate.value=""
         this.listEvents(this.myArr)
-        this.globalArr = []
+        this.globalArr = this.myArr;
+        this.updateGlobalArr();
+        
 
+        //Erikas Klass
+        this.eventListItems = Array.from(document.getElementsByClassName("event-list-item"));  
+        this.eventListItemsOpen;
+        this.trackOpenCards();   
+
+    }
+
+    updateGlobalArr() {
+        let sortBtn = document.getElementById("filter-button");
+        sortBtn.addEventListener("click", (e) => {
+            this.eventListItems = Array.from(document.getElementsByClassName("event-list-item")); 
+            console.log(this.globalArr);
+        });
+    }
+
+
+    //Erikas Metoder
+    trackOpenCards() {
+        for (let i = 0; i < this.eventListItems.length; i++) {
+            this.eventListItems[i].addEventListener("click", (e) => {
+                if (this.eventListItems[i].classList.contains("open")) {
+                    this.removeEventListDetails(i);
+                    this.eventListItems[i].classList.remove("open");
+  
+                } else {
+                    this.eventListItems[i].classList.add("open");
+                    this.printEventDetails();
+                }                
+            })
+        }       
+    }
+
+    printEventDetails() {
+
+        for(let i = 0; i < this.eventListItems.length; i++) {
+
+            if(this.eventListItems[i].classList.contains("open")) {
+            //Ta bort current nodes
+            let eventListName = document.getElementsByClassName("event-list-name");
+            eventListName[i].firstChild.classList.add("hidden");
+
+            //Skapa Element
+            //let localStorageEvents = new LocalStorageHandler();
+            //let eventsArray = localStorageEvents.getStoredArray();
+            let categoryCompanyHeader = document.createElement("H3")
+            let aboutHeader = document.createElement("H4")
+            let aboutP = document.createElement("P")
+            let locationP = document.createElement("P")
+            let participantsP = document.createElement("P")
+            let administratorP = document.createElement("P")
+            let eventManagerP = document.createElement("P")
+            let startsP = document.createElement("P")
+            let endsP = document.createElement("P")
+            let paragrphs = document.createElement("DIV")
+            let aboutContainer = document.createElement("DIV");
+
+            let imgDiv = document.createElement("DIV")
+            let guestBookHeader = document.createElement("H4")
+            let guestBookText = document.createElement("TEXTAREA")
+            let sendButton = document.createElement("BUTTON")
+
+            let mainContainer = document.createElement("DIV")
+            let leftColumn = document.createElement("DIV")
+            let rightColumn = document.createElement("DIV")
+
+            //Ge Event info:
+            categoryCompanyHeader.textContent = this.globalArr[i].category + ", " + this.globalArr[i].companyName;
+            aboutHeader.textContent = "About: "
+            aboutP.textContent = this.globalArr[i].infoText;
+            locationP.textContent = "Location: " + this.globalArr[i].location;
+            participantsP.textContent = "Participants: " + this.globalArr[i].participants;
+            administratorP.textContent = "Administrator: " + this.globalArr[i].admin;
+            eventManagerP.textContent = "Event Manager: " + this.globalArr[i].eventManager;
+            startsP.textContent = "Starts: " + this.globalArr[i].startDate + ", " + this.globalArr[i].startTime;
+            endsP.textContent = "Ends: " + this.globalArr[i].endDate + ", " + this.globalArr[i].endTime;
+            
+            sendButton.innerHTML = "SEND";
+            guestBookHeader.innerText = "Guest Book";
+            
+            //Ge Classnames
+            mainContainer.classList.add("list-item-open");
+            leftColumn.classList.add("open-left-column");
+            rightColumn.classList.add("open-right-column");
+            categoryCompanyHeader.classList.add("open-main-header");
+            aboutHeader.classList.add("open-about-header");
+            aboutContainer.classList.add("open-about");
+            imgDiv.classList.add("detail-img");
+            paragrphs.classList.add("detail-paragraphs")
+            sendButton.classList.add("detail-send-button");
+            guestBookHeader.classList.add("detail-guest-header");
+            guestBookText.classList.add("detail-guest-text");
+
+            //Append Element
+            paragrphs.appendChild(startsP);
+            paragrphs.appendChild(endsP);
+            paragrphs.appendChild(eventManagerP);
+            paragrphs.appendChild(administratorP);
+            paragrphs.appendChild(participantsP);
+            paragrphs.appendChild(locationP);
+
+            aboutContainer.appendChild(aboutP);
+
+            leftColumn.appendChild(categoryCompanyHeader);
+            leftColumn.appendChild(aboutHeader);
+            leftColumn.appendChild(aboutContainer);
+            leftColumn.appendChild(paragrphs);
+        
+            rightColumn.appendChild(imgDiv);
+            rightColumn.appendChild(guestBookHeader);
+            rightColumn.appendChild(guestBookText);
+            rightColumn.appendChild(sendButton);
+        
+            mainContainer.appendChild(leftColumn);
+            mainContainer.appendChild(rightColumn);
+
+            this.eventListItems[i].childNodes[1].appendChild(mainContainer);
+            }           
+        } 
+    }
+
+    removeEventListDetails(i) {
+        this.eventListItems[i].childNodes[1].childNodes[2].remove();
+        let eventListName = document.getElementsByClassName("event-list-name");
+        for(let i = 0; i < eventListName.length; i++ ){
+            eventListName[i].firstChild.classList.remove("hidden");
+        }
     }
     
     listEvents(events){
@@ -161,14 +289,13 @@ class itemLister{
 let itemObj = new itemLister()
 
 
-
-let click = document.getElementById("filter-button").addEventListener("click", function(e){
-    let array = itemObj.sortArrayBy(itemObj.myArr, "startDate");
+//let click = 
+document.getElementById("filter-button").addEventListener("click", function(e){
     
     itemObj.globalArr = []
 
     itemObj.listallEvents(itemObj.myArr)
-    console.log(itemObj.globalArr[0])
+    //console.log(itemObj.globalArr[0])
 
     localStorage.removeItem("kevinsarray")
     localStorage.setItem("kevinsarray", JSON.stringify(itemObj.globalArr))
